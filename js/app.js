5f4cef4ffb390662cdefbc5d4da21d89cb7c8358
AlphaTech <gfranklings@gmail.com>
2019-11-16 09:59:45 -0400

    Variables globales
    Se definen las variables globales del app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // Variables globales
-
+let btnYellow = document.querySelector('#yellow'),
+	btnBlue = document.querySelector('#blue'),
+	btnRed = document.querySelector('#red'),
+	btnGreen = document.querySelector('#green'),
+	btnPurple = document.querySelector('#purple');
 
 // Classes
 
