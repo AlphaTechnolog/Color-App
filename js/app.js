895b79fe99ca9810fb0f569353f8b4652ad5bc43
AlphaTech <gfranklings@gmail.com>
2019-11-16 10:41:49 -0400

    Complete Code
    Se completó todo el código de el app.js

--- js/app.js
+++ js/app.js
@@ -1,47 +1,61 @@
+// Clases
+class SetBgPageColor {
+	constructor(bg, txColor) {
+		document.body.style.background = bg
+		document.body.style.color = txColor
+
+		// Guardando en el localStorage el color del bg y de las letras que se debe mostrar al recargar la página
+
+		this.guardarLocalStorage('bg-color', bg)
+		this.guardarLocalStorage('tx-color', txColor)
+	}
+
+	guardarLocalStorage(key, value) {
+		localStorage.setItem(key, value)
+	}
+}
+
+class SetPageBgLS {
+	constructor() {
+		if (localStorage.getItem('bg-color') && localStorage.getItem('tx-color')) {
+			document.body.style.background = localStorage.getItem('bg-color')
+			document.body.style.color = localStorage.getItem('tx-color')
+		} else {
+			document.body.style.background = 'white'
+			document.body.style.color = 'black'
+		}
+	}
+}
+
 // Variables globales
-let btnYellow = document.querySelector('#yellow'),
-	btnBlue = document.querySelector('#blue'),
-	btnRed = document.querySelector('#red'),
-	btnGreen = document.querySelector('#green'),
-	btnPurple = document.querySelector('#purple');
-
-// Classes
-class SetTxPageColor {
-	constructor(txColor) {
-		document.body.style.color = txColor;
-	}
-}
-
-class SetBGPageColor {
-	constructor(bgColor) {
-		if (bgColor != 'white') {
-			document.body.style.background = bgColor;
-			new SetTxPageColor('white');
-		}
-		else {
-			document.body.style.background = bgColor;
-			new SetTxPageColor('black');
-		}
-	}
-}
-
-class GuardarLS {
-	constructor(key, value) {
-		localStorage.setItem(key, value);
-	}
-}
-
-class ObtenerLS {
-	constructor(key) {
-		if (localStorage.getItem(key)) {
-			// Si existe el dato
-			return localStorage.getItem(key);
-		}
-		else {
-			// No existe el dato
-			throw `El dato que se intenta obtener no existe, dato: ${key}`;
-		}
-	}
-}
+let btnYellow = document.querySelector('#yellow');
+let btnBlue = document.querySelector('#blue');
+let btnRed = document.querySelector('#red');
+let btnGreen = document.querySelector('#green');
+let btnPurple = document.querySelector('#purple');
 
 // EventsListeners
+btnYellow.addEventListener('click', () => {
+	new SetBgPageColor('yellow', 'white');
+});
+
+btnBlue.addEventListener('click', () => {
+	new SetBgPageColor('blue', 'white');
+});
+
+btnRed.addEventListener('click', () => {
+	new SetBgPageColor('red', 'white');
+});
+
+btnGreen.addEventListener('click', () => {
+	new SetBgPageColor('green', 'white');
+});
+
+btnPurple.addEventListener('click', () => {
+	new SetBgPageColor('purple', 'white');
+});
+
+window.addEventListener('load', () => {
+	console.log("This page has been loaded successfull")
+	new SetPageBgLS();
+})
